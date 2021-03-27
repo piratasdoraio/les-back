30f46f0fbd5d1fbd69edb19becd5a35c10776eb5
raiomarco <raiomarco@piratasdoraio.com>
2021-03-27 17:51:15 -0300

    Criado sistema de roteamento para a API
    Co-Authored-By: Davi Correia <71407050+jdavicp@users.noreply.github.com>
    Co-Authored-By: Paulo Gustavo Araujo Santana <54332693+Ollke@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
+var paciente = require('./routes/paciente')
+var consulta = require('./routes/consulta')
+var medico = require('./routes/medico')
+var exame = require('./routes/exame')
+var laudo = require('./routes/laudo')
+var funcionario = require('./routes/funcionario')
+var admin = require('./routes/admin')
+var sistema = require('./routes/sistema')
+
+app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+app.use('/paciente', paciente)
+app.use('/consulta', consulta)
+app.use('/medico', medico)
+app.use('/exame', exame)
+app.use('/laudo', laudo)
+app.use('/funcionario', funcionario)
+app.use('/admin', admin)
+app.use('/sistema', sistema)
+
 app.listen(3030)
+
+console.log("rodando aq")
