bc9f44787841125694def830b687e577864f89b1
raiomarco <raiomarco@piratasdoraio.com>
2021-03-29 11:22:34 -0300

    Adicionado parte inicial do endpoint de cadastrar paciente
    Co-Authored-By: Paulo Gustavo Araujo Santana <54332693+Ollke@users.noreply.github.com>
    Co-Authored-By: Davi Correia <71407050+jdavicp@users.noreply.github.com>

--- routes/paciente.js
+++ routes/paciente.js
@@ -1,4 +1,7 @@
 var express = require('express')
+const db = require('../modules/db')
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 var router = express.Router()
 
 // // middleware that is specific to this router
@@ -7,8 +10,25 @@ var router = express.Router()
 //   next()
 // })
 
-router.post('/', (req, res) => {
-    res.json({ "message": "Em desenvolvimento" })
+router.post('/', async (req, res) => {
+    let paciente = await prisma.pacientes.create({
+        data: {
+            email: req.body.email,
+            senha: req.body.senha,
+            nome: req.body.nome,
+            telefone: req.body.telefone,
+            cpf: req.body.cpf,
+            enderecos: {
+                create: {
+                    cep: req.body.endereco.cep,
+                    complemento : req.body.endereco.complemento,
+                    numero: req.body.endereco.numero,
+                }
+            },
+            nascimento: new Date(req.body.nascimento)
+        }
+    })
+    res.json({ "message": `Paciente ${paciente.nome} criado com o id ${paciente.id}` })
 })
 
 router.post('/login', (req, res) => {
