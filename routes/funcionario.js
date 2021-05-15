0420c1541cf683d9ba1b28ebe3151204ae2e122f
Paulo Gustavo Araujo Santana <54332693+Ollke@users.noreply.github.com>
2021-05-15 17:50:29 -0300

    Adcionado cadastro e login de funcionario

--- routes/funcionario.js
+++ routes/funcionario.js
@@ -8,12 +8,87 @@ var router = express.Router()
 //   next()
 // })
 
-router.post('/', (req, res) => {
-    res.json({ "message": "Em desenvolvimento" })
+function verifyJWT(req, res, next) {
+    const token = req.headers['x-access-token'];
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+
+    jwt.verify(token, process.env.SECRET, function (err, decoded) {
+        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+
+        // se tudo estiver ok, salva no request para uso posterior
+        req.userId = decoded.id;
+        next();
+    });
+}
+
+router.post('/', async (req, res) => {
+    data = req.body
+
+    let senha = require("crypto")
+        .createHash("sha256")
+        .update(data.senha)
+        .digest("hex");
+
+    let funcionario = await db.funcionarios.create({
+        data: {
+            email: data.email,
+            senha: senha,
+            nome: data.nome,
+            cpf: data.cpf,
+            telefone: data.telefone,
+            enderecos: {
+                create: {
+                    cep: data.endereco.cep,
+                    complemento: data.endereco.complemento,
+                    numero: data.endereco.numero,
+                    }
+                },
+        }
+    })
+    axios.post("https://piratasdoraio.com/pdr-med/mailer/index.php", {
+        "token": "les-pdr-senha-yey",
+        "de": "PDR MED",
+        "email": funcionario.email,
+        "assunto": "Conta criada",
+        "mensagem": `Olá ${funcionario.nome}, sua conta foi registrada com o id ${funcionario.id}`
+    })
+    res.json({ "message": `Funcionario ${funcionario.nome} criado com o id ${funcionario.id}` })
 })
 
-router.post('/login', (req, res) => {
-    res.json({ "message": "Em desenvolvimento" })
+router.post('/login', async (req, res) => {
+    let data = req.body
+    let senha = require("crypto")
+        .createHash("sha256")
+        .update(data.senha)
+        .digest("hex");
+    let funcionario = await db.funcionarios.findFirst({
+        where: {
+            email: {
+                equals: data.email
+            },
+            senha: {
+                equals: senha
+            }
+        }
+    })
+
+    if (!funcionario) return res.json({ "code": 3, "message": "Erro no login: Credenciais invalidas" })
+
+
+    const token = jwt.sign({
+            "code": 1,
+            "id": funcionario.id
+        },
+        "TODO super secreta senha TODO", {
+            // expiresIn: 300 // expires in 5min
+        });
+
+    res.json({
+        "code": 1,
+        "nome": funcionario.nome,
+        "id": funcionario.id,
+        "token": token
+    })
 })
 
 module.exports = router
