449fef3ba9927951a66dbcaeab269ff752fed481
José <jdavi.fac@gmail.com>
2021-06-15 19:17:28 -0300

    Listar exames e cadastrar exames
    Co-Authored-By: Matrix2100 <42838209+Matrix2100@users.noreply.github.com>
    Co-Authored-By: Paulo Gustavo Araujo Santana <54332693+Ollke@users.noreply.github.com>
    Co-Authored-By: raiomarco <16154130+raiomarco@users.noreply.github.com>

--- routes/exame.js
+++ routes/exame.js
@@ -7,10 +7,29 @@ var router = express.Router()
 //   console.log('Time: ', Date.now())
 //   next()
 // })
-
-router.post('/', (req, res) => {
+router.get('/:id', async(req, res)=>{
+    let id = parseInt(req.params.id)
+    let exame = await db.exames.findMany({
+        where: {
+            consulta: id,
+        },
+        orderBy: {
+            id: 'desc',
+        }    
+    }) 
+    
+    res.json({ exame: exame})
+})
+router.post('/', async (req, res) => {
     // #swagger.tags = ['exame']
-    res.json({ "message": "Em desenvolvimento" })
+    let exame = await db.exames.create({data: {
+        consultas: {connect: {id: req.body.consulta}},
+        data: req.body.data,
+        tipo: req.body.tipo,
+        status: "Marcado"
+    }})
+
+    res.json({exame: exame})
 })
 
 router.post('/:id', (req, res) => {
